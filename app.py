f93d371068edb072eb6711dd659edcbf9df325dd
Hasham Akram <hashamakram50@gmail.com>
2024-01-16 10:41:03 +0500

    algorithm and hyperparameter tuning done

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import sys
+
 from src.MLproject.exception import CustomException
 from src.MLproject.logger import logging
-from src.MLproject.components.data_ingestion import DataIngestion
-from src.MLproject.components.data_ingestion import DataIngestionConfig
+from src.MLproject.components.data_ingestion import DataIngestion,DataIngestionConfig
 from src.MLproject.components.data_transformation import DataTransformationconfig,DataTransformation
+from src.MLproject.components.model_trainer import ModelTrainerConfig,ModelTrainer 
+
 
 if __name__=="__main__":
     logging.info("The Execution has started")
@@ -13,7 +15,12 @@ if __name__=="__main__":
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
      
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+        ## Model Training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
 
     except Exception as e:
         logging.info("Custom Exception")    
